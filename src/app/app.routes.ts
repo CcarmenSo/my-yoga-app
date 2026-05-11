45acddad38e765e06de802253063306cfef82bf8
Maria del Carmen Soriano Muñoz <carmen.soriano@altia.es>
2026-05-11 16:51:07 +0200

    style: improve Tailwind styling across the app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,19 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {path: "home",
+  {
+    path: "home",
     loadComponent: () => import('./features/home/home.component')
   },
-  {path: "levels",
+  {
+    path: "levels",
     loadComponent: () => import('./features/levels/levels.component')
   },
-  {path: "categories",
+  {
+    path: "categories",
     loadComponent: () => import('./features/categories/categories.component')
   },
-   {
+  {
     path: '**',
     redirectTo: 'dashboard'
   }
